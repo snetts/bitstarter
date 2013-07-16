9289fbdb90d5db1517eeb6c29a32b5bf13d65fbe
Victor Okech <snettsblog@snetts.com>
2013-07-16 08:24:34 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+var fs = require('fs');
+
+/*app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
+*/
+
+fs.writeFile('index.html', 'Okay Hello World from index.html', function (app) {
+  if (app) throw app;
+  console.log('The data was saved to file');
+});
+ 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
