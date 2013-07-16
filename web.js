ee1b82592ba3ce0b5b2404e9fecc2bdd0b96fc47
Victor Okech <snettsblog@snetts.com>
2013-07-16 08:40:06 +0000

    Updated web.js version 1.0

--- web.js
+++ web.js
@@ -1,19 +1,27 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
 
-/*app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-*/
-
-fs.writeFile('index.html', 'Okay Hello World from index.html', function (app) {
+fs.writeFile('index.html', 'Hello World from index.html', function (app) {
   if (app) throw app;
   console.log('The data was saved to file');
 });
- 
+
+fs.readFile("index.html", function(err, html) {
+	if(err) {
+		throw err;
+	}
+	app.get('/', function(request, response) {
+	
+	response.writeHeader(200, {"Content-Type": "text/html"});
+	response.write(html);
+	response.end();
+});
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
